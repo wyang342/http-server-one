44e0d4c6ec7b5a2e53791eadceb51c32a6e42ed7
jalletto <joshalletto@gmail.com>
2019-02-04 15:51:37 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 9292))
 print('Waiting For Connection...')
-server.listen(1)
+server.listen()
 
-client_connection, client_address = server.accept()
+client_connection, _client_address = server.accept()
 print('New Connection received!')
 
 client_connection.close()
