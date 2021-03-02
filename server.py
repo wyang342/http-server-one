f4267301d714f3b5e2556043b3624f664c01513e
Chad Mowbray <49203942+Chad-Mowbray@users.noreply.github.com>
2021-03-02 15:28:00 -0600

    enable port reuse

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # so you don't have to change ports when restarting
 server.bind(('localhost', 9292))
 print('Waiting For Connection...')
 server.listen()
